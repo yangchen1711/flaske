143a18eaecbbbac5560eff78b8a03eb1110b3da0
yangchen1711 <yangchen1711@163.com>
2017-12-18 21:18:08 +0800

    完成用户级别的资料编辑及显示

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,10 @@
-from flask import render_template, session, redirect, url_for, current_app
+from flask import render_template, session, redirect, url_for, current_app, flash
 from .. import db
 from ..models import User
 from ..email import send_email
 from . import main
-from .forms import NameForm
+from .forms import NameForm, EditProfileForm
+from flask_login import login_required, current_user
 
 
 @main.route('/', methods=['GET', 'POST'])
@@ -33,3 +34,21 @@ def user(username):
     # if user is None:
     #     abort(404)
     return render_template('user.html', user=user)
+
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
